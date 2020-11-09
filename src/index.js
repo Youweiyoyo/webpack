207d2216a3dca5f76b603d442790f87292dbd22f
Youwei <1176071733@qq.com>
2020-11-09 12:33:36 +0800

    webpack

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import $ from "jquery";
+import "./css/1.css";
+import "../images/1.jpg";
 $(function () {
   $("li:odd").css("backgroundColor", "cyan");
   $("li:even").css("backgroundColor", "pink");
+})
+// -------------------------------
+import Vue from 'vue';
+// 定义单文件组件
+import App from "./components/App.vue"
+const vm = new Vue({
+  el: '#app',
+  render: h => h(App)
 })
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ const htmlPlugin = new HtmlWebpackPlugin({
   template: "./src/index.html",
   //设置生成的预览页面名称
   filename: "index.html"
-})
+});
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 module.exports = {
   //可以设置为development(开发模式)，production(发布模式)
   mode: "development",
@@ -19,5 +20,13 @@ module.exports = {
     //设置文件名
     filename: "bundle.js"
   },
-  plugins: [htmlPlugin]
+  plugins: [htmlPlugin, new VueLoaderPlugin()],
+  module: {
+    rules: [
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      { test: /\.jpg|png|gif|bmp|ttf|eot|svg|woff|woff2$/, use: 'url-loader?limit=17000' },
+      { test: /\.vue$/, loader: 'vue-loader' }
+    ]
+  }
+
 }
